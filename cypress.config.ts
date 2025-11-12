e4edf9fa1099c934de957fd74b9ac2d2bf17e6f3
Ian McPhail <ian.mcphail@method.com>
2025-11-12 13:20:10 -0500

    feat: Set up test coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,12 @@
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import coverageTask from '@cypress/code-coverage/task';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.ts',
-    specPattern: ['cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', '**/*.feature'],
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions
@@ -23,13 +21,23 @@ export default defineConfig({
         })
       );
 
+      // Register the code coverage task
+      coverageTask(on, config);
+
       // Make sure to return the config object as it might have been modified by the plugin.
       return config;
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: ['cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', '**/*.feature'],
     video: false,
-    screenshotOnRunFailure: true,
     viewportWidth: 1280,
     viewportHeight: 720
+  },
+  env: {
+    codeCoverage: {
+      url: 'http://localhost:3000/__coverage__',
+      expectFrontendCoverageOnly: true
+    }
   },
   component: {
     devServer: {
@@ -43,6 +51,11 @@ export default defineConfig({
           }
         }
       }
+    },
+    setupNodeEvents(on, config) {
+      // Register the code coverage task
+      coverageTask(on, config);
+      return config;
     },
     specPattern: 'src/components/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/component.ts',
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    istanbul({
+      cypress: true,
+      extension: ['.js', '.ts', '.jsx', '.tsx'],
+      include: 'src/**/*',
+      requireEnv: false,
+      nycrcPath: './.nycrc.json',
+      forceBuildInstrument: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
