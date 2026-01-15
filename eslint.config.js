59e9cd4738e8b36cca5d0c85f06bfcd781e32a47
Ian McPhail <ian.mcphail@method.com>
2026-01-15 11:38:53 -0500

    chore: Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -19,10 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
+      'react-hooks/incompatible-library': 'off',
+      'react-refresh/only-export-components': 'off',
       '@typescript-eslint/no-namespace': 'off'
     }
   }
